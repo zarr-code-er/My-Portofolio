1e52c1889ce8ca1e8da98bbfc3e2cf76ae9cce23
zarr-code-er <nizargeming28@gmail.com>
2025-03-21 20:45:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
